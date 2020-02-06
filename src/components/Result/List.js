b31c4bfba39919a4a1c8b5c07ee5c9b74aafcd92
Kacper <kacperjacekwasiak@gmail.com>
2020-02-06 23:03:49 +0100

    add condition to tabs names

--- src/components/Result/List.js
+++ src/components/Result/List.js
@@ -2,7 +2,6 @@ import React from "react";
 import ListItem from "./ListItem";
 
 const List = ({ data }) => {
-  console.log(data);
   const item = data.map(item => <ListItem key={item.id} {...item} />);
   return <ul>{item}</ul>;
 };
--- src/components/Result/ListItem.js
+++ src/components/Result/ListItem.js
@@ -2,8 +2,17 @@ import React from "react";
 import "./ListItem.css";
 
 const ListItem = ({ title, artist, tabTypes }) => {
-  const tab = tabTypes.map(item => <p key={item}>Tab: {item}</p>);
-  console.log(tabTypes);
+  const tab = tabTypes.map(item => (
+    <p className="item__tab" key={item}>
+      Tab:{" "}
+      {item === "TEXT_GUITAR_TAB"
+        ? "GUITAR"
+        : item === "TEXT_BASS_TAB"
+        ? "BASS"
+        : item}
+    </p>
+  ));
+
   return (
     <li className="item">
       <p className="item__artist">Artist: {artist.name}</p>
